fb633f670bacb61ceb5136c4eb37a179d57d9b81
akio0911 <akio0911@gmail.com>
2010-01-01 01:48:53 +0900

    modify add_plans date range

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 class Plan < ActiveRecord::Base
   def self.add_plans
-    start_time = Time.local(0,0,12,30,12,2009,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
+    start_time = Time.local(0,0,0,31,12,2009,0,0,false,'Asia/Tokyo')#Time.utc(2008, 12, 31, 12, 0, 'Asia/Tokyo')#08/12/31 12:00
 
-    0.upto(47) do |i|
+    0.upto(48) do |i|
       content = "未定"
       time = start_time + i.hours
       Plan.create(:content=> content, :start=> time)
