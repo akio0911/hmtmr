32236cab82736b293f47bdd931ddcf3b13bf0a3c
akio0911 <akio0911@gmail.com>
2010-01-01 00:28:23 +0900

    modify migrate file

--- db/migrate/002_create_statuses.rb
+++ db/migrate/002_create_statuses.rb
@@ -9,9 +9,6 @@ class CreateStatuses < ActiveRecord::Migration
       t.column :status_text, :string # <text>Reblog感覚でプレゼン</text>
       t.column :user_screen_name, :string # <screen_name>yuiseki</screen_name>      
 
-      t.column :float, :latitude # google apiで変換
-      t.column :float, :longitude # google apiで変換
-
       t.timestamps
     end
     add_index :statuses, [:status_id], :unique=>true
