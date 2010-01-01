23266492c55b55dab0579b73185317e3b0d00363
akio0911 <akio0911@gmail.com>
2010-01-01 22:09:25 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-#  map.root :controller=> "plans", :action=>'index'
-  map.connect '/all', :controller=>"plans", :action=>"all"
-  map.resources :plans
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -42,6 +38,4 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
